5843feb8e343bdd293aa1e74a4cd0a08b85a1c45
Nullean <mr000lean@gmail.com>
2021-04-18 03:12:35 +0400

    изменил верстку главной страницы

--- src/panels/Role.js
+++ src/panels/Role.js
@@ -1,42 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
-import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
-import PanelHeaderBack from '@vkontakte/vkui/dist/components/PanelHeaderBack/PanelHeaderBack';
-
 import {
+	Panel,
+	PanelHeader,
+	PanelHeaderBack,
 	Div,
 	Group,
 	Card,
-	Avatar,
-	RichCell,
-	PopoutWrapper,
-	ScreenSpinner
+	Avatar
 } from "@vkontakte/vkui";
-import teacher from '../icons/groups/teacher.png';
-import parent from '../icons/groups/parent.png';
-import student from '../icons/groups/student.png';
+import teacher from '../img/groups/teacher.png';
+import parent from '../img/groups/parent.png';
+import student from '../img/groups/student.png';
+import logo from '../img/logo.png';
 import './Role.css';
 import './Common.css';
 const Role = props => (
-	<Panel   id={props.id}>
-		<PanelHeader>
-			<div className = 'panelHeader'> Главная </div>
-		</PanelHeader>
-		<div className="PanelRole">
-			<Card className="CardRole"> <Avatar size="100%"  className="Role" src={teacher} onClick={props.go} data-to="home" alt="role teacher"/>
-				<div className="RoleText"> Я учитель</div>
-			</Card>
-			<Card className="CardRole"> <Avatar size="100%" className="Role" src={parent} onClick={props.go} data-to="home" alt="role parent"/>
-				<div className="RoleText"> Я родитель </div>
-			</Card>
-			<Card className="CardRole"> <Avatar size="100%" className="Role" src={student} onClick={props.go} data-to="studentProfile" alt="role student"/>
-				<div className="RoleText"> Я ученик </div>
-			</Card>
-
+	<Panel id={props.id}>
+		<div className='panelBox'>
+			<div className='panelHeader roleHeader'>
+				Кто Вы?
+			</div>
+			<div className="PanelRole">
+				<Card className="CardRole"> <Avatar size="100%"  className="Role" src={teacher} onClick={props.go} data-to="home" alt="role teacher"/>
+					<div className="RoleText"> Я учитель</div>
+				</Card>
+				<Card className="CardRole"> <Avatar size="100%" className="Role" src={parent} onClick={props.go} data-to="home" alt="role parent"/>
+					<div className="RoleText"> Я родитель </div>
+				</Card>
+				<Card className="CardRole"> <Avatar size="100%" className="Role" src={student} onClick={props.go} data-to="studentProfile" alt="role student"/>
+					<div className="RoleText"> Я ученик </div>
+				</Card>
+			</div>
+			<img src={logo} className='logoSmall' />
 		</div>
-
 	</Panel>
 );
 
