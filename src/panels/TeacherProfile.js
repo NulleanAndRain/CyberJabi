22328b5c75feaf763c6d6ea8be0bcca12aae9f2e
j-kachalova <j-kachalova@yandex.ru>
2021-04-18 07:23:23 +0400

    учитель экран 1

--- src/panels/TeacherProfile.js
+++ src/panels/TeacherProfile.js
@@ -17,19 +17,14 @@ import './StudentProfile.css';
 import './Common.css';
 import froge from '../img/groups/frog.png';
 import course0 from '../img/math.jpg';
-import course1 from '../img/courses/image2.png';
-import course2 from '../img/courses/image3.png';
-import course3 from '../img/courses/3dsMax.png';
-import roadmap from '../img/roadmap.png';
-import miniFrog1 from '../img/icons/checkFrog3.png';
-import miniFrog2 from '../img/icons/checkFrog4.png';
+import course1 from '../img/math2.jpg';
 
 const TeacherProfile = props => (
 	<Panel id={props.id}>
 		<PanelHeader
 			left={<PanelHeaderBack onClick={props.go} data-to="role"/>}
 		> 
-			<div className = 'panelHeader'> Ученик </div>
+			<div className = 'panelHeader'> Учитель </div>
 		</PanelHeader>
 		<Group>
 			{props.fetchedUser &&
@@ -45,63 +40,19 @@ const TeacherProfile = props => (
 		</Group>
 		<Group>
 			<CardScroll>
-				<Card className="courseCard" onClick={props.go} data-to='studentCourse'>
+				<Card className="courseCardTeacher" onClick={props.go} data-to='studentCourse'>
 					<img className='courceImg' id="img0" src={course0} />
 					<Div className='courseName'>
 						Математика для новичков
 					</Div>
-					<div className='progressWrapper' id="progress0">
-						<div className="checkFrog" id='checkFrog0'>
-							<img src={miniFrog1} />
-							<img src={miniFrog2} />
-						</div>
-					</div>
-					<div className="weeks" > 4 недели из 10 </div>
-					<div className="scores" > 69 баллов </div>
 				</Card>
-				<Card className="courseCard">
-					<img className='courceImg' id="img1" src={course1} />
+				<Card className="courseCardTeacher" onClick={props.go} data-to='studentCourse'>
+					<img className='courceImg' id="img0" src={course1} />
 					<Div className='courseName'>
-						Курс по CSS
+						Дискретная математика
 					</Div>
-					<div className='progressWrapper' id="progress1">
-						<div className="checkFrog" id='checkFrog1'>
-							<img src={miniFrog1} />
-							<img src={miniFrog2} />
-						</div>
-					</div>
+				</Card>
 
-					<div className="weeks" > 7 недель из 40 </div>
-					<div className="scores" > 40 баллов </div>
-				</Card>
-				<Card className="courseCard">
-					<img className='courceImg' id="img2" src={course2} />
-					<Div className='courseName'>
-						Работа в Figma
-					</Div>
-					<div className='progressWrapper' id="progress2">
-						<div className="checkFrog" id='checkFrog2'>
-							<img src={miniFrog1} />
-							<img src={miniFrog2} />
-						</div>
-					</div>
-					<div className="weeks"> 27 недель из 40 </div>
-					<div className="scores" > 230 баллов </div>
-				</Card>
-				<Card className="courseCard">
-					<img className='courceImg'  src={course3} />
-					<Div className='courseName'>
-						Курс 3D моделирования
-					</Div>
-					<div className='progressWrapper' id="progress3">
-						<div className="checkFrog" id='checkFrog3'>
-							<img src={miniFrog1} />
-							<img src={miniFrog2} />
-						</div>
-					</div>
-					<div className="weeks" > 38 недель из 40 </div>
-					<div className="scores" > 478 баллов</div>
-				</Card>
 			</CardScroll>
 		</Group>
 	</Panel>
